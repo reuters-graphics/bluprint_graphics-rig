33381d326c89feb25e058ffc88fbe1996f2925c9
Jon McClure <jon.r.mcclure@gmail.com>
2020-02-05 11:28:15 -0500

    ejs templates in node modules

--- config/rules/dev/ejs/templated.js
+++ config/rules/dev/ejs/templated.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   test: /\.ejs$/,
-  include: path.resolve(__dirname, '../../../../src/js'),
+  include: [
+    path.resolve(__dirname, '../../../../src/js'),
+    path.resolve(__dirname, '../../../../node_modules'),
+  ],
   use: {
     loader: path.resolve(__dirname, '../../../loaders/ejs-loader/index.js'),
   },
