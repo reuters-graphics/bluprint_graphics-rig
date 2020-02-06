5c9a48e327f246216e96c5c025aff2176f08eb40
Jon McClure <jon.r.mcclure@gmail.com>
2020-02-05 19:22:03 -0500

    closes #8

--- bin/previewServer/index.js
+++ bin/previewServer/index.js
@@ -4,9 +4,12 @@ const path = require('path');
 const open = require('open');
 const ngrok = require('ngrok');
 const chalk = require('chalk');
+const getLocales = require('../../config/utils/getLocales');
 const logger = require('../../config/utils/logger')('Preview server');
 
 const argv = require('yargs').argv;
+
+const locales = getLocales();
 
 portfinder.basePort = 8000;
 
@@ -17,16 +20,17 @@ portfinder.getPortPromise()
       open: false,
       root: path.resolve(__dirname, '../../dist'),
     });
-    open(`http://localhost:${port}/en/`);
 
-    logger.info(chalk`⚡ Server started at {yellow ${`http://localhost:${port}`}}\n`);
+    locales.forEach(locale => open(`http://localhost:${port}/${locale}/`, { background: true }));
+
+    logger.info(chalk`⚡ Server started at {yellow ${`http://localhost:${port}/en/`}}\n`);
 
     // If passing --ngrok, we'll open up a tunnel
     if (argv.ngrok) {
       const url = await ngrok.connect({
         addr: port,
       });
-      open(`${url}/en/`, { background: true });
-      logger.info(chalk`⚡ Ngrok preview at {yellow ${url}}`);
+      locales.forEach(locale => open(`${url}/${locale}/`, { background: true }));
+      logger.info(chalk`⚡ Ngrok preview at {yellow ${url}/en/}`);
     }
   });
