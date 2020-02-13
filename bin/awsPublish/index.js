16b8239eb0ec28ce05ac309b28fb177e99d0c56f
Jon McClure <jon.r.mcclure@gmail.com>
2020-02-13 09:41:21 -0500

    aws cache

--- bin/awsPublish/index.js
+++ bin/awsPublish/index.js
@@ -13,24 +13,48 @@ const setPackageProp = require('../../config/utils/setPackageProp');
 const DIST_ROOT = path.resolve(__dirname, '../../dist/');
 const BUCKET_NAME = 'graphics.thomsonreuters.com';
 
-const s3 = new AWS.S3();
+const LOGFILE = path.join(process.cwd(), '.aws.publish.log');
 
-const getHash = () => {
-  const { awsPreviewHash } = getPackageProp('reuters');
+const s3 = new AWS.S3({ region: 'us-east-1' });
 
-  if (awsPreviewHash) return awsPreviewHash;
-
-  const hash = cryptoRandomString({ length: 12, type: 'url-safe' }).replace(/[^A-Za-z0-9]/g, '');
-  setPackageProp('reuters.awsPreviewHash', hash);
-  return hash;
+const getLog = () => {
+  if (!fs.existsSync(LOGFILE)) fs.writeFileSync(LOGFILE, '{}');
+  return JSON.parse(fs.readFileSync(LOGFILE, 'utf-8'), (k, v) => {
+    return typeof v === 'string' ? new Date(v) : v;
+  });
 };
 
-const uploadFile = async(relativePath, hash) => {
+const writeLog = () => fs.writeFileSync(LOGFILE, JSON.stringify(log));
+
+const getURI = () => {
+  const { awsPreviewURI } = getPackageProp('reuters');
+
+  if (awsPreviewURI) return awsPreviewURI;
+
+  const hash = cryptoRandomString({ length: 12, type: 'url-safe' }).replace(/[^A-Za-z0-9]/g, '');
+  const URI = `${new Date().getFullYear()}/${hash}`;
+  setPackageProp('reuters.awsPreviewURI', URI);
+  return URI;
+};
+
+const uploadFile = async(relativePath, uri) => {
   const absolutePath = path.resolve(DIST_ROOT, relativePath);
+
+  const currentMtime = fs.statSync(absolutePath).mtime;
+
+  const lastMTime = log[relativePath];
+
+  if (lastMTime && currentMtime <= lastMTime) {
+    logger.info(chalk`{green.dim Skip} {yellow.dim ${relativePath}}`);
+    return;
+  } else {
+    logger.info(chalk`{green.dim Send} {yellow.dim ${relativePath}}`);
+    log[relativePath] = currentMtime;
+  }
 
   const fileContent = fs.readFileSync(absolutePath);
 
-  const bucketPath = path.join('testfiles', hash, relativePath);
+  const bucketPath = path.join('testfiles', uri, relativePath);
 
   const params = {
     Bucket: BUCKET_NAME,
@@ -51,16 +75,22 @@ const uploadFile = async(relativePath, hash) => {
 
 const uploadDist = async() => {
   const files = glob.sync('**/*', { cwd: DIST_ROOT, nodir: true });
-  const hash = getHash();
+  const uri = getURI();
 
   logger.info('Uploading dist directory to AWS...');
   for (const i in files) {
     const file = files[i];
-    logger.info(chalk` {yellow.dim ${file}}`);
-    await uploadFile(file, hash);
+    await uploadFile(file, uri);
   }
 
-  await open(`https://graphics.thomsonreuters.com/testfiles/${hash}/en/`);
+  writeLog();
+
+  const URL = `https://graphics.thomsonreuters.com/testfiles/${uri}/en/`;
+
+  await open(URL);
+  logger.info(chalk`Page published at:\n\n {green ${URL}}\n`);
 };
+
+const log = getLog();
 
 uploadDist();
