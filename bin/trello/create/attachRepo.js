addc1aa07fb627d83528316980f4e132f1ad6a1a
Jon McClure <jon.r.mcclure@gmail.com>
2020-04-18 10:22:31 +0100

    manually reformat github URL

--- bin/trello/create/attachRepo.js
+++ bin/trello/create/attachRepo.js
@@ -2,7 +2,6 @@ const os = require('os');
 const fs = require('fs');
 const path = require('path');
 const gitRemoteOriginUrl = require('git-remote-origin-url');
-const hostedGitInfo = require('hosted-git-info');
 const axios = require('axios');
 
 module.exports = async(cardId) => {
@@ -11,8 +10,7 @@ module.exports = async(cardId) => {
   const { trelloApiKey: key, trelloApiToken: token } = JSON.parse(credFile);
 
   const gitOrigin = await gitRemoteOriginUrl();
-  const info = hostedGitInfo.fromUrl(gitOrigin);
-  const url = info.docs();
+  const url = gitOrigin.replace('https://api.github.com/repos', 'https://github.com');
   const name = 'GitHub';
 
   await axios.post(`https://api.trello.com/1/cards/${cardId}/attachments`,
