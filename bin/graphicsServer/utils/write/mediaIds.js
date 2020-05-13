f7494c17e8408335034bc11ccd6e3a96d0cd95d3
Jon McClure <jon.r.mcclure@gmail.com>
2020-05-13 12:51:40 +0100

    hotfix for server change

--- bin/graphicsServer/utils/write/mediaIds.js
+++ bin/graphicsServer/utils/write/mediaIds.js
@@ -1,8 +1,8 @@
 module.exports = (editions, fileId, write) => {
   const mediaEditions = editions.filter(d => d.file.fileId === fileId);
 
-  const { id: mediaId } = mediaEditions.filter(d => d.title === 'media-interactive')[0];
-  const { id: interactiveId } = mediaEditions.filter(d => d.title === 'interactive')[0];
+  const { id: mediaId } = mediaEditions.filter(d => d.editionName === 'media-interactive')[0];
+  const { id: interactiveId } = mediaEditions.filter(d => d.editionName === 'interactive')[0];
 
   write('editions.media.media-interactive.id', mediaId);
   write('editions.media.interactive.id', interactiveId);
