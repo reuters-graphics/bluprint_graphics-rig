442ad1a5256f8ad98e8951bb915943113ca74b57
Manas Sharma <manas.sharma@thomsonreuters.com>
2020-05-28 15:08:12 +0800

    add new template. rename. comment out code for replacing script file

--- bin/checkAi2html/constants.js
+++ bin/checkAi2html/constants.js
@@ -3,11 +3,11 @@ const os = require('os');
 
 module.exports = {
   github: {
-    TEMPLATE: 'Reuters-Template/ai2hmtl-template_newrig.ait',
-    SCRIPT: 'ai2html_newrig.js',
+    TEMPLATE: 'Reuters-Template/ai2hmtl-template.ait',
+    // SCRIPT: 'ai2html_newrig.js',
   },
   system: {
     TEMPLATE: path.resolve(__dirname, '../../project-files/ai2html-template.ait'),
-    SCRIPT: path.resolve(os.homedir(), 'Applications/Adobe Illustrator 2020/Presets/en_US/Scripts/ai2html_newrig.js'),
+    // SCRIPT: path.resolve(os.homedir(), 'Applications/Adobe Illustrator 2020/Presets/en_US/Scripts/ai2html_newrig.js'),
   },
 };
