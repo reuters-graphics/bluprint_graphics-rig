349179c3de2b20c0d9b3aa4caecc73f41f34dd9f
Jon McClure <jon.r.mcclure@gmail.com>
2020-10-07 20:01:44 +0100

    Pre-archive

--- bin/buildPackages/copyDocs.js
+++ bin/buildPackages/copyDocs.js
@@ -4,18 +4,22 @@ const ejs = require('ejs');
 const getLocaleProp = require('../../config/utils/getLocaleProp');
 
 const ROOT = path.resolve(__dirname, '../../');
+const README_TEMPLATE = path.join(__dirname, 'docs/README.txt');
+const EMBED_TEMPLATE = path.join(__dirname, 'docs/EMBED.html');
+const MEDIA_NOTES = path.join(ROOT, 'MEDIA_NOTES.txt');
 
 module.exports = (locale) => {
   const localeProp = getLocaleProp(locale);
   // Embed code
   const url = localeProp('editions.media.interactive.url');
-  const template = fs.readFileSync(path.resolve(__dirname, 'docs/EMBED.html'), 'utf-8');
-  const filepath = path.resolve(ROOT, `packages/${locale}/media-interactive/EMBED.html`);
-  fs.mkdirSync(path.dirname(filepath), { recursive: true });
-  fs.writeFileSync(filepath, ejs.render(template, { url }));
-  // README
-  fs.copyFileSync(
-    path.resolve(__dirname, 'docs/README.txt'),
-    path.resolve(ROOT, `packages/${locale}/media-interactive/README.txt`)
-  );
+
+  const embed = fs.readFileSync(EMBED_TEMPLATE, 'utf-8');
+  const readme = fs.readFileSync(README_TEMPLATE, 'utf-8');
+  const notes = fs.readFileSync(MEDIA_NOTES, 'utf-8');
+
+  const embedCode = `Use the following code to embed this project in your CMS:\n\n${ejs.render(embed, { url })}\n\n`;
+
+  const filepath = path.resolve(ROOT, `packages/${locale}/media-interactive/README.txt`);
+
+  fs.writeFileSync(filepath, embedCode + readme + notes);
 };
