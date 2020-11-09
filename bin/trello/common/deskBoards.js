4a204d43832bcd17fc1b6b9f5093386e97fc495d
Jon McClure <jon.r.mcclure@gmail.com>
2020-11-09 18:59:13 +0000

    westside desk trello

--- bin/trello/common/deskBoards.js
+++ bin/trello/common/deskBoards.js
@@ -1,5 +1,4 @@
 module.exports = [
-  { title: 'AMER', value: '5e9756520beb9c595326a820' },
   { title: 'APAC', value: '5e98988c3fdc280f040663ac' },
-  { title: 'EMEA', value: '5e9898431c06c024777bca26' },
+  { title: 'Westside', value: '5e9898431c06c024777bca26' },
 ];
