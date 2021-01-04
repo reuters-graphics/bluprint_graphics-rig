5f912fa393b52ebbb2919188f6519001b65c049a
hobbes7878 <jon.r.mcclure@gmail.com>
2021-01-04 19:34:08 +0000

    fixes some meta stuff

--- bin/checkMeta/package/index.js
+++ bin/checkMeta/package/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const chalk = require('chalk');
 const path = require('path');
 const askJSON = require('ask-json');
+const yargs = require('yargs/yargs');
 const JSONschema = require('./schema');
 const logger = require('../../../config/utils/logger')('Check meta');
 
@@ -12,12 +13,14 @@ const checkPackage = async(locales) => {
   const metadata = JSON.parse(fs.readFileSync(filePath));
   logger.info('Package metadata.');
 
-  if (metadata.reuters.publishDate) {
+  const argv = yargs(process.argv).argv;
+
+  if (metadata.reuters.publishDate && !argv.silent) {
     JSONschema.properties.reuters.required.push('updateDate');
     delete metadata.reuters.updateDate;
   }
 
-  const data = await askJSON(JSONschema, metadata, { askToAddItems: true });
+  const data = await askJSON(JSONschema, metadata, { askToAddItems: !argv.silent });
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
   logger.info(`✏️  If you need to edit this data, do it in ${chalk.yellow('package.json')}.`);
 };
