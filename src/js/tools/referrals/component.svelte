018ea75c81fc62e0a7c5a733868bc23e010eb258
hobbes7878 <jon.r.mcclure@gmail.com>
2021-06-04 11:26:45 +0100

    link referalls to latest JSON

--- src/js/tools/referrals/component.svelte
+++ src/js/tools/referrals/component.svelte
@@ -1,9 +1,19 @@
 <script>
-import metadata from './metadata.json';
+import { onMount } from 'svelte';
 import Link from './Link.svelte';
 import Badge from './Badge.svelte';
+
+let metadata;
+
+onMount(() => {
+  fetch('https://graphics.thomsonreuters.com/data/reuters-graphics/homepage/latest.json')
+    .then(resp => resp.json())
+    .then((data) => {
+      metadata = data.slice(0, 4).map(({ url, image, title, description }) => ({ url, image, title, description }));
+    });
+});
 </script>
-{#if metadata.length > 0}
+{#if metadata}
 <section class='referral-container'>
   <Badge />
   <nav class='referral-rail row'>
